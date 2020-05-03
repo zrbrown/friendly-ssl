b49513b7d26ab0156c278764a1d7b1ee49cc6cc4
Zack Brown <zackrbrown@gmail.com>
2020-05-03 19:46:03 -0400

    added new controller tests, minor fixup

--- src/main/java/net/eightlives/friendlyssl/controller/TermsOfServiceController.java
+++ src/main/java/net/eightlives/friendlyssl/controller/TermsOfServiceController.java
@@ -2,7 +2,9 @@ package net.eightlives.friendlyssl.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import net.eightlives.friendlyssl.config.FriendlySSLConfig;
+import net.eightlives.friendlyssl.exception.SSLCertificateException;
 import net.eightlives.friendlyssl.service.TermsOfServiceService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +34,10 @@ public class TermsOfServiceController {
             return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body("URI could not be created from terms link " + termsLink);
+        } catch (SSLCertificateException e) {
+            log.error("Exception occurred while writing to terms of service file for terms link " + termsLink, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Exception occurred while writing to terms of service file for terms link " + termsLink);
         }
     }
 }
