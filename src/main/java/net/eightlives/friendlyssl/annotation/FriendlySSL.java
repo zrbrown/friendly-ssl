76abc1ebff68d32e85b0ab30f596fb65fc94e4d1
Zack Brown <zack.brown@equipmentshare.com>
2020-05-09 18:28:45 -0400

    added @EnableScheduling to annotation

--- src/main/java/net/eightlives/friendlyssl/annotation/FriendlySSL.java
+++ src/main/java/net/eightlives/friendlyssl/annotation/FriendlySSL.java
@@ -10,6 +10,7 @@ import net.eightlives.friendlyssl.listener.ChallengeTokenRequestedListener;
 import net.eightlives.friendlyssl.listener.FriendlySSLApplicationListener;
 import net.eightlives.friendlyssl.service.*;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.lang.annotation.*;
 
@@ -37,5 +38,6 @@ import java.lang.annotation.*;
         UpdateCheckerService.class,
         AccountBuilderFactory.class
 })
+@EnableScheduling
 public @interface FriendlySSL {
 }
