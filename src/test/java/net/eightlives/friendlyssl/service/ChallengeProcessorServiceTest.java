f0af8c3bdae632d50ffd68af12e40386744967a1
Zack Brown <zack.brown@equipmentshare.com>
2020-05-17 22:41:04 -0400

    improved challenge token logic

--- src/test/java/net/eightlives/friendlyssl/service/ChallengeProcessorServiceTest.java
+++ src/test/java/net/eightlives/friendlyssl/service/ChallengeProcessorServiceTest.java
@@ -1,52 +1,40 @@
 package net.eightlives.friendlyssl.service;
 
-import net.eightlives.friendlyssl.config.FriendlySSLConfig;
 import net.eightlives.friendlyssl.exception.SSLCertificateException;
 import net.eightlives.friendlyssl.listener.ChallengeTokenRequestedListener;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.shredzone.acme4j.AcmeJsonResource;
 import org.shredzone.acme4j.Authorization;
 import org.shredzone.acme4j.Status;
 import org.shredzone.acme4j.challenge.Http01Challenge;
-import org.shredzone.acme4j.exception.AcmeException;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.*;
-import java.util.function.Supplier;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ChallengeProcessorServiceTest {
 
-    private static final String CHALLENGE_TOKEN = "this is a token";
-    private static final String CHALLENGE_AUTH = "this is an authorization";
-
     private ChallengeProcessorService service;
 
     @Mock
-    private FriendlySSLConfig config;
-    @Mock
-    private ChallengeTokenStore challengeTokenStore;
-    @Mock
     private ChallengeTokenRequestedListener challengeTokenRequestedListener;
-    @Mock
-    private UpdateCheckerService updateCheckerService;
 
     @BeforeEach
     void setUp() {
-        service = new ChallengeProcessorService(config, challengeTokenStore,
-                challengeTokenRequestedListener, updateCheckerService);
+        service = new ChallengeProcessorService(challengeTokenRequestedListener);
     }
 
     @DisplayName("Valid authorizations should be filtered out")
@@ -89,111 +77,62 @@ class ChallengeProcessorServiceTest {
         @Nested
         class HttpChallenges {
 
-            @BeforeEach
-            void setUp() {
-                when(challenge.getToken()).thenReturn(CHALLENGE_TOKEN);
-                when(challenge.getAuthorization()).thenReturn(CHALLENGE_AUTH);
-            }
-
-            @DisplayName("when challenge trigger throws exception")
+            @DisplayName("when challengeTokenRequestedListener throws exception")
             @Test
-            void triggerException() throws AcmeException {
-                doThrow(new AcmeException()).when(challenge).trigger();
+            void listenerException() {
+                when(challengeTokenRequestedListener.getChallengeTokenVerification(challenge, auth))
+                        .thenThrow(new SSLCertificateException(new RuntimeException()));
 
                 assertThrows(SSLCertificateException.class, () -> service.process(List.of(auth)));
+            }
+
+            @DisplayName("when all processed authorizations complete successfully")
+            @Test
+            void futuresComplete() {
+                when(challengeTokenRequestedListener.getChallengeTokenVerification(challenge, auth))
+                        .thenReturn(CompletableFuture.completedFuture(null));
+
+                assertTrue(service.process(List.of(auth)).isDone());
             }
 
             @DisplayName("when trigger succeeds")
             @Nested
             class TriggerSucceeds {
 
-                @Mock
-                private CompletableFuture<ScheduledFuture<Void>> challengeListener;
-
-                private final Map<String, String> tokenStoreTokens = new HashMap<>(Map.of(CHALLENGE_TOKEN, CHALLENGE_AUTH));
+                private final CompletableFuture<Void> challengeFuture = new CompletableFuture<>();
 
                 @BeforeEach
                 void setUp() {
-                    when(config.getTokenRequestedTimeoutSeconds()).thenReturn(3);
-                    when(challengeTokenRequestedListener.setTokenRequestedListener(matches(CHALLENGE_TOKEN), any()))
-                            .thenReturn(challengeListener);
-                    when(challengeTokenStore.getTokens()).thenReturn(tokenStoreTokens);
+                    when(challengeTokenRequestedListener.getChallengeTokenVerification(challenge, auth))
+                            .thenReturn(challengeFuture);
                 }
 
-                @DisplayName("and challenge listener fails")
-                @ParameterizedTest(name = "with exception {0}")
-                @ValueSource(classes = {TimeoutException.class, InterruptedException.class, ExecutionException.class, CancellationException.class})
-                void challengeListenerFails(Class<Throwable> exceptionClass) throws ExecutionException, InterruptedException, TimeoutException {
-                    when(challengeListener.get(3, TimeUnit.SECONDS)).thenThrow(exceptionClass);
+                @DisplayName("and challenge future fails")
+                @Test
+                void challengeFutureFails() {
+                    challengeFuture.completeExceptionally(new SSLCertificateException(new RuntimeException()));
 
-                    assertThrows(SSLCertificateException.class, () -> service.process(List.of(auth)));
+                    Exception e = assertThrows(ExecutionException.class, () -> service.process(List.of(auth)).get(1, TimeUnit.SECONDS));
+                    assertTrue(e.getCause() instanceof SSLCertificateException);
                 }
 
-                @DisplayName("and challenge listener succeeds")
-                @Nested
-                class ChallengeListenerSucceeds {
+                @DisplayName("and challenge future times out")
+                @Test
+                void challengeFutureTimeout() {
+                    challengeFuture.completeExceptionally(new TimeoutException());
 
-                    @Mock
-                    private ScheduledFuture<Void> updateChecker;
-
-                    @BeforeEach
-                    void setUp() throws InterruptedException, ExecutionException, TimeoutException {
-                        when(challengeListener.get(3, TimeUnit.SECONDS)).thenReturn(updateChecker);
-                        when(config.getAuthChallengeTimeoutSeconds()).thenReturn(2);
-                    }
-
-                    @DisplayName("and update checker fails")
-                    @ParameterizedTest(name = "with exception {0}")
-                    @ValueSource(classes = {TimeoutException.class, InterruptedException.class, ExecutionException.class, CancellationException.class})
-                    @Timeout(value = 2)
-                    void updateCheckerFails(Class<Throwable> exceptionClass) throws InterruptedException, ExecutionException, TimeoutException {
-                        when(updateChecker.get(2, TimeUnit.SECONDS)).thenThrow(exceptionClass);
-
-                        CompletableFuture<Void> responseFuture = service.process(List.of(auth));
-
-                        CountDownLatch latch = new CountDownLatch(1);
-                        responseFuture.handle((aVoid, throwable) -> {
-                            assertTrue(throwable.getCause() instanceof SSLCertificateException);
-                            return Void.TYPE;
-                        }).thenRun(latch::countDown);
-                        latch.await();
-                    }
-
-                    @DisplayName("and update checker succeeds")
-                    @Test
-                    void updateCheckerSucceeds() throws InterruptedException {
-                        CompletableFuture<Void> responseFuture = service.process(List.of(auth));
-
-                        CountDownLatch latch = new CountDownLatch(1);
-                        responseFuture.thenRun(latch::countDown);
-                        latch.await();
-
-                        assertTrue(responseFuture.isDone());
-                    }
+                    Exception e = assertThrows(ExecutionException.class, () -> service.process(List.of(auth)).get(1, TimeUnit.SECONDS));
+                    assertTrue(e.getCause() instanceof TimeoutException);
                 }
 
-                @AfterEach
-                void tearDown() {
-                    assertFalse(tokenStoreTokens.containsKey(CHALLENGE_TOKEN));
+                @DisplayName("and challenge future succeeds")
+                @Test
+                void challengeFutureSucceeds() throws ExecutionException, InterruptedException, TimeoutException {
+                    challengeFuture.complete(null);
+
+                    CompletableFuture<Void> future = service.process(List.of(auth));
+                    assertTrue(future.isDone());
                 }
-            }
-
-            @AfterEach
-            void tearDown() throws AcmeException {
-                InOrder inOrder = inOrder(challengeTokenStore, challengeTokenRequestedListener, challenge);
-
-                inOrder.verify(challengeTokenStore, times(1)).setToken(CHALLENGE_TOKEN, CHALLENGE_AUTH);
-
-                ArgumentCaptor<Supplier<ScheduledFuture<Void>>> listenerArg = ArgumentCaptor.forClass(Supplier.class);
-                inOrder.verify(challengeTokenRequestedListener, times(1)).setTokenRequestedListener(
-                        matches(CHALLENGE_TOKEN), listenerArg.capture()
-                );
-                ScheduledFuture<Void> updateFuture = mock(ScheduledFuture.class);
-                when(updateCheckerService.start(any(AcmeJsonResource.class))).thenReturn(updateFuture);
-                listenerArg.getValue().get();
-                verify(updateCheckerService, times(1)).start(auth);
-
-                inOrder.verify(challenge, times(1)).trigger();
             }
         }
     }
